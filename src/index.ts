77b3a29803830c82601055d87f26569db7b70e09
Timothy Ahn <tahn2023@gmail.com>
2025-07-01 18:50:52 -0500

    domain restrict

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,45 @@ import express from 'express';
 import http from 'http';
 import cors from 'cors';
 import router from './routes';
-import {initializeClosesCache} from './streaming/closingcache';
-import {initprices} from './streaming/current';
+import { initializeClosesCache } from './streaming/closingcache';
+import { initprices } from './streaming/current';
 import { cryptostream } from './streaming/cryptosocket';
-import { stockstream} from './streaming/stocksocket';
+import { stockstream } from './streaming/stocksocket';
 import { setupWebSocket } from './websocket'; 
 import { getNews } from './news';
 
+// Stream services
 stockstream();
 cryptostream();
 
 const app = express();
 const server = http.createServer(app);
-app.use(cors());
-app.use(express.json());
 
+// CORS options with domain restriction
+const corsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = ['https://buegr.com'];
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // Allow if no origin (e.g. when testing locally)
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));  // Use the restricted CORS options
+
+// Middlewares
+app.use(express.json());
 app.use('/api', router);
 
-setupWebSocket(server); 
+// WebSocket setup
+setupWebSocket(server);
 
 const PORT = process.env.PORT || 5001;
 server.listen(PORT, async () => {
   console.log(`Server running on port ${PORT}`);
   await initializeClosesCache(); // ensures cache is ready at boot
-  await initprices(); //fetches latest prices at server start
-  await getNews(); //gathers news
+  await initprices(); // fetches latest prices at server start
+  await getNews(); // gathers news
 });
