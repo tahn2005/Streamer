8f459968e3ac22787271e86bc23b47a209dc00eb
Timothy Ahn <tahn2023@gmail.com>
2025-07-10 13:04:19 -0500

    db

--- src/routes.ts
+++ src/routes.ts
@@ -12,6 +12,7 @@ let symbols = JSON.parse(process.env.SYMBOLS);
 function verifyToken(req, res, next) {
   const token = req.headers['authorization']?.split(' ')[1];
   if (!token || token !== process.env.TOKEN) {
+    console.log("invalid token");
     return res.status(401).json({ error: 'Unauthorized: Invalid or missing token' });
   }
   next();
